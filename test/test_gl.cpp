6594ad0bb6618cfbed7f673c58dc1297e592ab8e
chenchunyu <cbtxs@qq.com>
2024-01-12 11:41:59 +0800

    update

--- test/test_gl.cpp
+++ test/test_gl.cpp
@@ -2,18 +2,78 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+#include <fstream>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+  std::string vertex_shader_source;
+  std::string fragment_shader_source;
+};
+
+static ShaderProgramSource ParseShader(const std::string & file_path)
+{
+  enum class ShaderType
+  {
+    NONE = -1, VERTEX = 0, FRAGMENT=1
+  };
+
+  std::ifstream stream(file_path);
+  std::stringstream ss[2];
+  std::string line;
+  ShaderType type;
+  while(getline(stream, line))
+  {
+    if(line.find("#shader") != std::string::npos)
+    {
+      if(line.find("vertex")!=std::string::npos)
+        type = ShaderType::VERTEX;
+      if(line.find("fragment")!=std::string::npos)
+        type = ShaderType::FRAGMENT;
+    }
+    else
+    {
+      ss[(uint8_t)type] << line << "\n";
+    }
+  }
+  return {ss[0].str(), ss[1].str()};
+}
+
+static uint32_t CompileShader(uint32_t type, const std::string & source)
+{
+  uint32_t shader = glCreateShader(type);
+  const char * src = source.c_str();
+  glShaderSource(shader, 1, &src, nullptr);
+  glCompileShader(shader);
+
+  /** 检查是否有编译错误 */
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(shader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  return shader;
+}
+
+static uint32_t CreateShader(const std::string & vshader, const std::string & fshader)
+{
+  uint32_t program = glCreateProgram();
+  uint32_t vs = CompileShader(GL_VERTEX_SHADER, vshader);
+  uint32_t fs = CompileShader(GL_FRAGMENT_SHADER, fshader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+  return program;
+}
 
 void framebuffer_size_callback(GLFWwindow * , int width, int height)
 {
@@ -30,7 +90,7 @@ void draw_a_triangle()
 {
 }
 
-int main()
+int main(int , char ** argv)
 {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); /**< 告诉 GLFW GL 的主版本号 */
@@ -54,61 +114,25 @@ int main()
     return -1;
   }
 
-  /** 定义顶点着色器 GL_VERTEX_SHADER 代表顶点着色器*/
-  uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);/**< 编译着色器 */
-
-  /** 检查着色器是否编译错误 */ 
-  int success;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  if (!success)
-  {
-      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-  }
-
-  /** 定义片段着色器 GL_FRAGMENT_SHADER 代表片段着色器*/
-  uint32_t fragmentShader;
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  /** 检查着色器是否编译错误 */ 
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  if (!success)
-  {
-      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-  }
-
-  uint32_t shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-  /** check for linking errors */
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if (!success) {
-      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-      std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-  }
-
-  //glDeleteShader(vertexShader);
-  //glDeleteShader(fragmentShader);
+  ShaderProgramSource source = ParseShader(argv[1]);
+  uint32_t shader = CreateShader(source.vertex_shader_source, source.fragment_shader_source);
+  glUseProgram(shader);
 
   /** set up vertex data (and buffer(s)) and configure vertex attributes */
   float vertices[] = {
       -0.5f, -0.5f, 0.0f, // left  
        0.5f, -0.5f, 0.0f, // right 
-       0.0f,  0.5f, 0.0f  // top   
+       0.5f,  0.5f, 0.0f,  // top   
+      -0.5f,  0.5f, 0.0f  // top   
   }; 
 
   unsigned int VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(GL_ELEMENT_ARRAY_BUFFER, VAO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, VAO)
+
+  glGenBuffers(1, &VBO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -116,13 +140,6 @@ int main()
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-  //glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-  //glBindVertexArray(0); 
 
   /** 定义视图窗口 */
   glViewport(0, 0, 800, 600);
@@ -138,12 +155,15 @@ int main()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     /** 绘制窗口 */
     glfwSwapBuffers(window);
 
     /** 检测有没有发生鼠标移动，键盘输入等事件 */
     glfwPollEvents();
   }
+  glDeleteProgram(shader);
   glfwTerminate();
   return 0;
 }
